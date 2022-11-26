6e1692fc904e9912f4c70fa9663153b6994a99ef
seleniumthoughts <vinayakumarc11@gmail.com>
2022-11-27 00:33:05 +0530

    Controllers Added

---
+++ WebGentle_BookStore/WebGentle_BookStore/Controllers/BookController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebGentle_BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        public string GetAllBooks()
+        {                                               // Note: In URL Method Names, parameter names are NOT CASE SESITIVE
+            //http://localhost:58860/book/getAllbooks      {controller}/{Method}
+            return "All Books";
+        }
+        public string GetBook(int id)
+        {
+            //http://localhost:58860/book/getbook/2      {controller}/{Method}/{id}
+            return $"Book Id ={id}";
+        }
+        public string SearchBooks(string bookName, string authorName)
+        {
+            //  http://localhost:58860/book/searchbooks?bookName=java&authorName=pqr   {controller}/{Method}?bookName=value&authorName=value
+            return $"Book Name={bookName} and Author Name={authorName}";
+        }
+    }
+}
--- WebGentle_BookStore/WebGentle_BookStore/Controllers/HomeController.cs
+++ WebGentle_BookStore/WebGentle_BookStore/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace WebGentle_BookStore.Controllers
     {
         public string Index()
         {
-            return "WebGentle";
+            return "WebGentle"; 
         }
     }
 }
